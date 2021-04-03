b3f4e0933622976161b3bc148cc1343de0501bd7
Rie <>
2021-04-04 02:08:50 +0900

    PHPmailerでpassなどを見えないようにするファイルを作成

---
+++ contact_test/PHPMailer/phpmailvars.php
@@ -0,0 +1,15 @@
+  <?php
+//SMTP サーバー（サーバーの場合：localhost でも可）
+define('MAIL_HOST', 'smtp.lolipop.jp');
+
+//PHPMailer を使って送信するための E-mail アカウント
+define('MAIL_USER', 'info@friekobo.com');
+
+//パスワード
+define('MAIL_PASSWORD', 'FRie8528-whone2');
+
+//送信先
+//define('SEND_TO', 'xxxx@xxxxxx.com');
+
+//送信先の名前
+//define('SEND_TO_NAME', '送信先名前');
